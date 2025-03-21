72b694c89236f13f034d6f803a768bc30ff56bc0
iamsavinashreddy <iamsavinashreddy@gmail.com>
2025-03-21 12:44:50 -0400

    Update images.ts

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -1,6 +1,6 @@
 export const images = {
-  logo: "../logo-1.jpeg",
-  hero: "../hero-1.jpeg",
+  logo: "./logo-1.jpeg",
+  hero: "./hero-1.jpeg",
   products: {
     rice: "/rice.jpg",
     flour: "/flour.jpg",
